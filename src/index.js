021253784c493c539373427aa23fc4169dd15eea
Abhik Banerjee <abhik.banerjee.1999@gmail.com>
2020-07-28 21:51:54 +0530

    Making commit before including OSS Service.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 const express = require('express')
+const path = require('path')
 const {AccessKeyID, AccessKeySecret} = require('../config/access_key.json')
 const app = express()
 const port = 3000
 
+
+var handlebars = require('express-handlebars')
+app.set('views', path.join(__dirname + '/views'))
+
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.set('view engine', '.hbs')
+app.engine('hbs', handlebars({
+    extname: 'hbs',
+    layoutsDir: __dirname + '/views/layouts'
+    
+    }));
 app.get('/', (req, res) => {
-    res.send
+    //  res.send(__dirname)
+    res.render('main', { layout: 'index'})
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
